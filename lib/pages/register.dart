83466cba27602ada9f2a16320c4cbc2a593f1d85
pr_Mais <hiimechi@gmail.com>
2020-03-24 20:30:19 +0300

    update register page

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 class RegisterPage extends StatefulWidget {
+  const RegisterPage();
   @override
   _RegisterPageState createState() => _RegisterPageState();
 }
@@ -8,50 +10,47 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 30),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'New Account',
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 50),
-            TextField(
+            const SizedBox(height: 50),
+            const TextField(
               decoration: InputDecoration(
-                  hintText: 'Email',
-                  prefixIcon: Icon(Icons.email)
-              ),
+                  hintText: 'Email', prefixIcon: Icon(Icons.email)),
             ),
-            SizedBox(height: 10),
-            TextField(
+            const SizedBox(height: 10),
+            const TextField(
               decoration: InputDecoration(
-                  hintText: 'Password',
-                  prefixIcon: Icon(Icons.lock)
-              ),
+                  hintText: 'Password', prefixIcon: Icon(Icons.lock)),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             RaisedButton(
               child: Text('Login'),
               color: Theme.of(context).primaryColor,
               onPressed: () {},
             ),
-            SizedBox(height: 60),
+            const SizedBox(height: 60),
             FlatButton(
               child: Text(
                 'Back to Login',
                 style: TextStyle(
                     color: Theme.of(context).primaryColor,
-                    fontWeight: FontWeight.bold
-                ),
+                    fontWeight: FontWeight.bold),
               ),
               highlightColor: Colors.amber[100],
-              splashColor:Colors.amber[50] ,
+              splashColor: Colors.amber[50],
               onPressed: () {
                 Navigator.of(context).pushNamed('/login');
               },
@@ -59,7 +58,6 @@ class _RegisterPageState extends State<RegisterPage> {
           ],
         ),
       ),
-
     );
   }
 }
